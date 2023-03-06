780584d577dbe4ef80a2bf2693adb2cac933edea
SalimRK <rizksalimrizk@gmail.com>
2023-03-06 23:25:52 +0200

    Create readability.py

---
+++ Week 6/5. sentimental-readability/readability.py
@@ -0,0 +1,34 @@
+# TODO
+
+count_letter = 0
+count_word = 1
+count_sentence = 0
+
+# getting input from user
+text = input("Text: ")
+text_length = len(text)
+
+# Count the number of letters in the text
+for i in range(text_length):
+    if(text[i].isalpha()):
+        count_letter += 1
+
+# Count the number of words in the text
+    if(text[i].isspace()):
+        count_word += 1
+
+# count the number of sentence
+    if(text[i] == '.' or text[i] == '?' or text[i] == '!'):
+        count_sentence += 1
+
+# calculate index
+calculation = (0.0588 * count_letter / count_word * 100) - (0.296 * count_sentence / count_word * 100) - 15.8
+
+index = round(calculation)
+
+if index < 1:
+    print("Before Grade 1")
+elif index > 16:
+    print("Grade 16+")
+else:
+    print(f"Grade {index}")
